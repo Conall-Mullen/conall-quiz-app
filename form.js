9253f6baf68441d3c767d9158d80ac82124566ef
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-19 17:18:39 +0200

    add comments for posterity

--- form.js
+++ form.js
@@ -1,18 +1,20 @@
 console.clear();
 console.log("running");
 
-const formMain = document.querySelector('[data-js="form-main"]');
-const form = document.querySelector('[data-js="form"]');
+const formMain = document.querySelector('[data-js="form-main"]'); //The main section of the html
+const form = document.querySelector('[data-js="form"]'); //The form
 const formSubmitButton = document.querySelector(
   '[data-js="form-submit-button"]'
 );
 
 function addNewQuestion(event) {
-  event.preventDefault();
+  event.preventDefault(); //Prevents page from refreshing when submit is clicked
+
   const formData = new FormData(event.target);
-  const data = Object.fromEntries(formData);
+  const data = Object.fromEntries(formData); //get readable data about from the form when submitted
   console.log(data);
 
+  /* All new elements must be declared in the function so that a new card is generated each time*/
   const newCard = document.createElement("section");
   newCard.classList.add("card");
 
@@ -40,8 +42,9 @@ function addNewQuestion(event) {
   newBookmarkPic.setAttribute("src", "resources/bookmark_transparent.png");
   newBookmarkPic.setAttribute("alt", "a bookmark icon");
 
-  formMain.append(newCard);
+  formMain.append(newCard); //append new card to main section
   newCard.append(
+    //append all children of card to card
     newQuestion,
     newAnswerButton,
     newAnswer,
@@ -49,6 +52,6 @@ function addNewQuestion(event) {
     newBookmarkButton
   );
 
-  newBookmarkButton.append(newBookmarkPic);
+  newBookmarkButton.append(newBookmarkPic); //appened picture to button
 }
-form.addEventListener("submit", addNewQuestion);
+form.addEventListener("submit", addNewQuestion); //Executes addNewQuestion when submit button is clicked
--- script.js
+++ script.js
@@ -9,9 +9,9 @@ const picButtonImage = document.querySelector(
 const answer = document.querySelector('[data-js="card-answer"]');
 
 function toggleAnswerButton() {
-  answer.classList.toggle("hidden");
-  //   console.log(answer.classList[1]);
-  answer.classList[1] == undefined
+  answer.classList.toggle("hidden"); //toggles the class "hidden" for the answer element which hides/reveals the text
+  // console.log(answer.classList);
+  answer.classList[1] == undefined //only when the class "hidden" appears in the answer element will the button show "reveal answer"
     ? (textButton.textContent = "Hide answer")
     : (textButton.textContent = "Reveal answer");
 }
