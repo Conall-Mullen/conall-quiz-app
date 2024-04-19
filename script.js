a951b4b9c31c0c6b5812fdaf8d83f37473776292
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-19 13:01:28 +0200

    add toggle function to pic button

--- script.js
+++ script.js
@@ -3,10 +3,28 @@ console.log("running");
 
 const textButton = document.querySelector('[data-js="card-text-button"]');
 const picButton = document.querySelector('[data-js="card-pic-button"]');
+const picButtonImage = document.querySelector(
+  '[data-js="card-pic-button-image"]'
+);
 const answer = document.querySelector('[data-js="card-answer"]');
 
-textButton.addEventListener("click", () => {
+function toggleAnswerButton() {
   answer.classList.toggle("hidden");
-});
+  //   console.log(answer.classList[1]);
+  answer.classList[1] == undefined
+    ? (textButton.textContent = "Hide answer")
+    : (textButton.textContent = "Reveal answer");
+}
 
-// picButton.addEventListener("click", toggleBookmarkButton);
+function togglePicButton() {
+  if (picButtonImage.alt == "a bookmark icon") {
+    picButtonImage.setAttribute("src", "resources/bookmark_filled.png");
+    picButtonImage.setAttribute("alt", "a filled bookmark icon");
+  } else {
+    picButtonImage.setAttribute("src", "resources/bookmark_transparent.png");
+    picButtonImage.setAttribute("alt", "a bookmark icon");
+  }
+}
+
+textButton.addEventListener("click", toggleAnswerButton);
+picButton.addEventListener("click", togglePicButton);
